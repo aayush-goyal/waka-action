76db72b2fd1d7d3ea1fa1a8e24e1976e09638e27
Aayush Goyal <aayushgoyal.mps@gmail.com>
2024-07-23 18:59:28 +0530

    Fix

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import core from '@actions/core';
+
+const API_BASE_URL = 'https://server-7hzpew6hia-el.a.run.app';
+// const svgData = axios.get(`${API_BASE_URL}/`);
+
+try {
+    const githubToken = core.getInput('GH_TOKEN');
+    console.log(githubToken);
+} catch (error) {
+    core.setFailed(error.message);
+}
