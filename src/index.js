9a519e977d8c5950e79e9a4a01c57560fba3c422
Aayush Goyal <aayushgoyal.mps@gmail.com>
2024-07-25 15:31:55 +0530

    Test #3

--- src/index.js
+++ src/index.js
@@ -10,39 +10,50 @@ try {
     const githubToken = core.getInput('GH_TOKEN');
     const workspace = core.getInput('GH_WORKSPACE');
     const mdFilePath = `${workspace}/README.md`;
-    console.log('PATH:', mdFilePath);
+    // console.log('PATH:', mdFilePath);
     const content2 = await fsPromises.readFile(mdFilePath, 'utf8');
+
+    content2.replace(/<!-- WAKAWAKA_CONFIG ST=\d&CT=\d&DT=\d&R=\d -->/, 'replaced');
+
+    console.log('LOG:', content2);
+
+//     <!-- WAKAWAKA_START -->
+// <!-- WAKAWAKA_CONFIG__ST=0&CT=0&DT=0&R=0 -->
+// <!-- WAKAWAKA_CONFIG ST=\d&CT=\d&DT=\d&R=\d -->
+// <!-- WAKAWAKA_END -->
+
+    const
 
     console.log('CONTENT:', content2);
 
-    const currentWorkingDirectory = dirname(fileURLToPath(import.meta.url));
-    // const mdFilePath = join(currentWorkingDirectory, '../utils/langs.json');
+    // const currentWorkingDirectory = dirname(fileURLToPath(import.meta.url));
+    // // const mdFilePath = join(currentWorkingDirectory, '../utils/langs.json');
 
-    // Get the GitHub token from the environment
-    const token = core.getInput('repo-token');
-    const octokit = github.getOctokit(token);
+    // // Get the GitHub token from the environment
+    // const token = core.getInput('repo-token');
+    // const octokit = github.getOctokit(token);
 
-    // Get the repo information from the context
-    const { owner, repo } = github.context.repo;
+    // // Get the repo information from the context
+    // const { owner, repo } = github.context.repo;
 
-    // Define the path to the MD file you want to access
-    const filePath = 'path/to/your/file.md';
+    // // Define the path to the MD file you want to access
+    // const filePath = 'path/to/your/file.md';
 
-    // Fetch the file content
-    const response = await octokit.rest.repos.getContent({
-        owner,
-        repo,
-        path: filePath
-    });
+    // // Fetch the file content
+    // const response = await octokit.rest.repos.getContent({
+    //     owner,
+    //     repo,
+    //     path: filePath
+    // });
 
-    // The content is base64 encoded, so decode it
-    const content = Buffer.from(response.data.content, 'base64').toString(
-        'utf8'
-    );
+    // // The content is base64 encoded, so decode it
+    // const content = Buffer.from(response.data.content, 'base64').toString(
+    //     'utf8'
+    // );
 
-    console.log(content);
+    // console.log(content);
 
-    console.log('aayush');
+    // console.log('aayush');
 } catch (error) {
     core.setFailed(error.message);
 }
