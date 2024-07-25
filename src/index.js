1cb4cdeebf24342c007cd54fc1334059be72f348
Aayush Goyal <aayushgoyal.mps@gmail.com>
2024-07-25 16:07:00 +0530

    First working code.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import core from '@actions/core';
 import { promises as fsPromises } from 'fs';
 import { dirname, join } from 'path';
@@ -6,57 +7,61 @@ import { fileURLToPath } from 'url';
 const API_BASE_URL = 'https://server-7hzpew6hia-el.a.run.app';
 // const svgData = axios.get(`${API_BASE_URL}/`);
 
+function getStatType(statType) {
+    switch (statType) {
+        case 0:
+            return 'languages';
+        case 1:
+            return 'categories';
+        case 2:
+            return 'editors';
+        case 3:
+            return 'os';
+        default:
+            return 'projects';
+    }
+}
+
 try {
     const githubToken = core.getInput('GH_TOKEN');
+    const wakaToken = core.getInput('WAKA_AUTH_TOKEN');
     const workspace = core.getInput('GH_WORKSPACE');
     const mdFilePath = `${workspace}/README.md`;
-    // console.log('PATH:', mdFilePath);
-    const content2 = await fsPromises.readFile(mdFilePath, 'utf8');
 
-    const replaced = content2.replace(
-        /<!-- WAKAWAKA_CONFIG__ST=\d&CT=\d&DT=\d&R=\d -->/,
-        'replaced'
-    );
+    const mdContent = await fsPromises.readFile(mdFilePath, 'utf8');
+    const configRegex = /<!-- WAKAWAKA_CONFIG__ST=\d&CT=\d&DT=\d&R=\d -->/g;
+    const configs = mdContent.match(configRegex);
 
-    console.log('LOG:', replaced);
+    for (let config of configs) {
+        const regex =
+            /<!-- WAKAWAKA_CONFIG__ST=(\d)&CT=(\d)&DT=(\d)&R=(\d) -->/;
 
-    //     <!-- WAKAWAKA_START -->
-    // <!-- WAKAWAKA_CONFIG__ST=0&CT=0&DT=0&R=0 -->
-    // <!-- WAKAWAKA_CONFIG ST=\d&CT=\d&DT=\d&R=\d -->
-    // <!-- WAKAWAKA_END -->
+        // Match the string against the regex and extract the captured groups
+        const queryParams = configs.match(regex);
+        console.log('PARAMS: ', queryParams);
 
-    // const
+        if (queryParams) {
+            // Extracted digit values are in the matches array starting from index 1
+            const statType = getStatType(matches[1]);
+            const chartType = matches[2];
+            const dataType = matches[3];
+            const range = matches[4];
 
-    // console.log('CONTENT:', content2);
+            const chartSVG = await axios.get(
+                `https://server-7hzpew6hia-el.a.run.app/charts/${statType}?range=${range}&chart_type=${chartType}&data_type=${dataType}&token=${wakaToken}`
+            );
 
-    // const currentWorkingDirectory = dirname(fileURLToPath(import.meta.url));
-    // // const mdFilePath = join(currentWorkingDirectory, '../utils/langs.json');
+            console.log(
+                `ST: ${statType}, CT: ${chartType}, DT: ${dataType}, R: ${range}`
+            );
 
-    // // Get the GitHub token from the environment
-    // const token = core.getInput('repo-token');
-    // const octokit = github.getOctokit(token);
+            console.log();
 
-    // // Get the repo information from the context
-    // const { owner, repo } = github.context.repo;
-
-    // // Define the path to the MD file you want to access
-    // const filePath = 'path/to/your/file.md';
-
-    // // Fetch the file content
-    // const response = await octokit.rest.repos.getContent({
-    //     owner,
-    //     repo,
-    //     path: filePath
-    // });
-
-    // // The content is base64 encoded, so decode it
-    // const content = Buffer.from(response.data.content, 'base64').toString(
-    //     'utf8'
-    // );
-
-    // console.log(content);
-
-    // console.log('aayush');
+            console.log('SVG', chartSVG);
+        } else {
+            console.log(`No query params provided in ${config}`);
+        }
+    }
 } catch (error) {
     core.setFailed(error.message);
 }
